8d5159ff745ffd0344d51852188449f4fcb8a689
Sergey Savelyev <sergey.savelyev@nextgames.com>
2023-10-02 20:00:10 +0300

    WIP [2]

--- src/MCGAssignment.TodoList.Lib/DataTransferObjects/LogEntryView.cs
+++
@@ -1,5 +0,0 @@
-using MCGAssignment.TodoList.Lib.Enums;
-
-namespace MCGAssignment.TodoList.Lib.DataTransferObjects;
-
-public record LogEntryView(Guid Id, TaskAction Action, long TimestampMsec, Guid? EntityId, string EntityType, string? Payload);
